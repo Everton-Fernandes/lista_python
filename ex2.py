8411cde34a5de62347e1fffd42a71d5c43c04a6f
Everton-Fernandes <e.everton.elf@gmail.com>
2025-03-24 09:52:11 -0300

    correção ex2.

--- ex2.py
+++ ex2.py
@@ -1,7 +1,6 @@
 def ordenar_por_idade(lista):
-    return sorted(lista, key=lambda pessoa: pessoa[1])
+    return sorted(lista, key=lambda item: item[1])
 
-# Exemplo de uso
 pessoas = [("Samuel", 25), ("Karynne", 22), ("Carol", 20), ("Kleber", 23), ("Vinicius", 27)]
 resultado = ordenar_por_idade(pessoas)
 
